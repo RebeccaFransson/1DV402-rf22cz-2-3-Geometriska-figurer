58be9bc4aab5c7ce1e13adef71c9c1e894acbd5b
rebeccafransson <rf222cz@student.lnu.se>
2014-10-13 15:52:23 +0200

    Klass rec, ell, shape klara

---
+++ Gemometriskafigurer/Gemometriskafigurer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemometriskafigurer
+{
+    public enum ShapeType { Rectangle, Ellipse };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static Shape CreateShape(ShapeType shapeType) //läser in objektets längd och bredd
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Gemometriskafigurer/Gemometriskafigurer/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemometriskafigurer
+{
+    class Rectangle : Shape 
+    {
+        public double Area // Räknar ut arean
+        {
+            get { return Length * Width; }
+        }
+
+        public double Perimeter // Räknar ut omkretsen
+        {
+            get { return ((2 * Length) + (2 * Width)); }
+        }
+
+        public Rectangle(double length, double width)
+            : base(length, width)
+        {
+
+        }
+    }
+}

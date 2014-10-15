5d500a5cff12bd0ea0d211713310963a01034129
rebeccafransson <rf222cz@student.lnu.se>
2014-10-15 11:41:22 +0200

    påbörjat metoder i program

---
+++ Gemometriskafigurer/Gemometriskafigurer/Ellipse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemometriskafigurer
+{
+    class Ellipse : Shape
+    {
+        public double Area // Räknar ut arean
+        {
+            get { return Math.PI * (Length / 2) * (Width / 2); }
+        }
+
+        public double Perimeter // räknar ut omkretsen
+        {
+            get { return Math.PI*Math.Sqrt(2*(Length / 2)*(Length / 2)  +  2*(Width / 2)*(Width / 2));}
+        }
+
+        public Ellipse(double area, double perimeter) 
+            :base(area, perimeter)
+        {
+
+        }
+    }
+}
--- Gemometriskafigurer/Gemometriskafigurer/Program.cs
+++ Gemometriskafigurer/Gemometriskafigurer/Program.cs
@@ -10,13 +10,48 @@ namespace Gemometriskafigurer
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Anropa viewmenu. Ej avsluta ->Createchape anropas
         {
+            //do
+            //{
+            //    switch(ViewMenu())
+            //    {
+
+            //    }
+
+            //}
+
         }
 
         private static Shape CreateShape(ShapeType shapeType) //läser in objektets längd och bredd
         {
-            Console.ReadLine();
+            input = Console.ReadLine();
+
+            
+        }
+
+       private double ReadDoubleGraterThanZero(string prompt)
+        {
+
+        }
+
+        private void ViewMenu()
+       {
+           Console.WriteLine(" ╔═══════════════════════════════════╗ ");
+           Console.WriteLine(" ║          Choose a form            ║ ");
+           Console.WriteLine(" ╚═══════════════════════════════════╝ ");
+           Console.BackgroundColor = ConsoleColor.Black;
+           Console.WriteLine(" 0. Exit.");
+           Console.WriteLine(" 1. Rectangle.");
+           Console.WriteLine(" 2. Ellipse.");
+           Console.WriteLine("\n ═══════════════════════════════════════════\n");
+           Console.Write(" Ange menyval [0-2]: ");
+           Console.ResetColor();
+       }
+
+        private void ViewShapeDetail(Shape shape)
+        {
+
         }
     }
 }
